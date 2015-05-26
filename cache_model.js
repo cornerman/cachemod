610543ed174f9f82ef679fd4a3d428de0ad18a0a
johannes karoff <johannes@karoff.net>
2015-05-26 13:15:51 +0200

    basic CacheModel for restmod

---
+++ cache_model.js
@@ -0,0 +1,52 @@
+angular.module("cachemod", ["restmod"]);
+
+angular.module("cachemod").provider("CacheModel", CacheModel);
+
+CacheModel.$inject = [];
+
+function CacheModel() {
+    this.$get = get;
+    this.setCache = setCache;
+
+    let cacheServiceName;
+    let store = {};
+    let defaultCache = {
+        set: (k,v) => store[k] = v,
+        get: _.propertyOf(store)
+    };
+
+    function setCache(serviceName) {
+        cacheServiceName = serviceName;
+    }
+
+    get.$inject = ["restmod", "$injector"];
+    function get(restmod, $injector) {
+        let cacheService = cacheServiceName ? $injector.get(cacheServiceName) : defaultCache;
+
+        return {
+            $extend: {
+                Model: {
+                    "$find": function(_pk) {
+                        let url = `${this.$url()}/${_pk}`;
+                        return cachedResponse.bind(this)(url, arguments);
+                    }
+                },
+                Collection: {
+                    "$fetch": function() {
+                        return cachedResponse.bind(this)(this.$url(), arguments);
+                    }
+                }
+            }
+        };
+
+        function cachedResponse(url, args) {
+            let cached = cacheService.get(url);
+            if (cached === undefined) {
+                cached = this.$super.apply(this, args);
+                cacheService.set(url, cached);
+            }
+
+            return cached;
+        }
+    }
+}
